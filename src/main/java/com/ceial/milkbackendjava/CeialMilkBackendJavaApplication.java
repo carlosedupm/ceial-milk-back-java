9504ff4beb2260c8fb091598fe0ad718b398b9a1
carlosmrpn <158525347+carlosmrpn@users.noreply.github.com>
2024-11-11 21:43:37 -0300

    Project created

---
+++ src/main/java/com/ceial/milkbackendjava/CeialMilkBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.ceial.milkbackendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeialMilkBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeialMilkBackendJavaApplication.class, args);
+	}
+
+}
